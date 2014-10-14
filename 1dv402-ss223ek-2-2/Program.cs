4a9643561392fabc00eaeac5c2ee65cd522c60a8
ss223ek <ss223ek@student.lnu.se>
2014-10-14 22:33:51 +0200

    Alla metoder klara
    nu ska main påbörjas
    
    Signed-off-by: ss223ek <ss223ek@student.lnu.se>

--- 1dv402-ss223ek-2-2/Program.cs
+++ 1dv402-ss223ek-2-2/Program.cs
@@ -8,8 +8,46 @@ namespace _1dv402_ss223ek_2_2
 {
     class Program
     {
+        //string HorizontalLine = "===============================================================";
         static void Main(string[] args)
         {
+            //skapa objekt
+            AlarmClock testClock = new AlarmClock();
+
         }
+        private static void Run(AlarmClock ac, int minutes)
+        {
+            for (int i = 0; i < minutes; i++)
+            {
+                bool alarm = ac.TickTock();
+                if (alarm)
+                {
+                    Console.WriteLine("Beep {0} Beep Beep!", ac.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("     {0}", ac.ToString());
+                }
+            }
+        }
+
+        private static void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+        }
+
+        private static void ViewTestHeader(string header)
+        {
+            Console.WriteLine("================================================================");
+            Console.WriteLine(header);
+        }
+
+
+
+
+
+
     }
 }
